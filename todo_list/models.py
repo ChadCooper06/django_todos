8b6339c4114bf913088697bd60d63e128f871bdf
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-04-13 02:25:55 +0000

    Added 2 classes-Todos and User

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Todos(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    priority = models.Choices(high,low)
+    completed = models.BooleanField()
+    created_by = models.CharField(max_length=200)
+    created_at = models.TimeField()
+    due_by = models.DateField()
+    category = models.Choices()
+
+    def __str__(self):
+        return self.name
